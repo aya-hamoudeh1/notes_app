4192c41c73e9225ae03108e1cdd3d8ffdeb91782
aya-hamoudeh1 <ayahamoudeh29@gmail.com>
2024-10-28 02:31:16 +0300

    create custom text field

--- lib/views/notes_view.dart
+++ lib/views/notes_view.dart
@@ -9,8 +9,12 @@ class NotesView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.green,
         onPressed: () {
           showModalBottomSheet(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(16),
+            ),
             context: context,
             builder: (context) {
               return const AddNoteButtonSheet();
--- lib/views/widgets/add_note_button_sheet.dart
+++ lib/views/widgets/add_note_button_sheet.dart
@@ -1,10 +1,21 @@
 import 'package:flutter/cupertino.dart';
+import 'package:notes_app/views/widgets/custom_text_field.dart';
 
 class AddNoteButtonSheet extends StatelessWidget {
   const AddNoteButtonSheet({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return const Padding(
+      padding: EdgeInsets.symmetric(horizontal: 16.0),
+      child: Column(
+        children: [
+          SizedBox(
+            height: 32,
+          ),
+          CustomTextField(),
+        ],
+      ),
+    );
   }
 }
