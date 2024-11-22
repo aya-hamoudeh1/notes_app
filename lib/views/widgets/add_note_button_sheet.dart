db15eb628a59dffc707d4389c8d4e47f4519e573
aya-hamoudeh1 <ayahamoudeh29@gmail.com>
2024-11-22 22:12:00 +0300

    create notes state

--- lib/views/widgets/add_note_button_sheet.dart
+++ lib/views/widgets/add_note_button_sheet.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:notes_app/cubits/add_note_cubit.dart';
+import 'package:notes_app/cubits/add_note_cubit/add_note_cubit.dart';
 import 'add_note_form.dart';
 
 class AddNoteButtonSheet extends StatelessWidget {
@@ -24,9 +24,12 @@ class AddNoteButtonSheet extends StatelessWidget {
         builder: (context, state) {
           return AbsorbPointer(
             absorbing: state is AddNoteLoading ? true : false,
-            child: const Padding(
-              padding: EdgeInsets.symmetric(horizontal: 16.0),
-              child: SingleChildScrollView(
+            child:  Padding(
+              padding: EdgeInsets.only(
+                  left: 16.0,
+                  right: 16,
+                  bottom: MediaQuery.of(context).viewInsets.bottom),
+              child: const SingleChildScrollView(
                 child: AddNotForm(),
               ),
             ),
