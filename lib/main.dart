08cd7a33216ce9271ea3d1202163f7b935075984
aya-hamoudeh1 <ayahamoudeh29@gmail.com>
2024-11-24 00:30:01 +0300

    add colors list view to edit note view

--- lib/main.dart
+++ lib/main.dart
@@ -5,18 +5,15 @@ import 'package:notes_app/constants.dart';
 import 'package:notes_app/models/note_model.dart';
 import 'package:notes_app/simple_bloc_observer.dart';
 import 'package:notes_app/views/notes_view.dart';
-
 import 'cubits/notes_cubit/notes_cubit.dart';
 
 void main() async {
   await Hive.initFlutter();
-
-  Bloc.observer= SimpleBlocObserver();
+  Bloc.observer = SimpleBlocObserver();
   Hive.registerAdapter(NoteModelAdapter());
   await Hive.openBox<NoteModel>(kNotesBox);
   runApp(const NotesApp());
 }
-
 
 class NotesApp extends StatelessWidget {
   const NotesApp({super.key});
